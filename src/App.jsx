a7ff356d1eb0c84098afcd3348790e3d2659bed0
subikstha <subsatch@gmail.com>
2024-06-19 20:53:23 +0545

    first commit for the wild oasis project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import styled from "styled-components"
+
+const H1 = styled.h1`
+  font-size: 40px;
+  font-weight: 500;
+`
+function App() {
+
+
+  return (
+    <>
+    <H1>I am a styled component</H1>
+    </>
+  )
+}
+
+export default App
