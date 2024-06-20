ad5f77f0aae792f14891f81d71d77771d9fbd677
subikstha <subsatch@gmail.com>
2024-06-20 15:37:16 +0545

    Created reusable button and row components using styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 
 import styled from "styled-components"
+import GlobalStyles from "./styles/GlobalStyles"
+import Button from "./ui/Button"
+import Input from "./ui/Input"
+import Heading from "./ui/Heading"
 
-const H1 = styled.h1`
-  font-size: 40px;
-  font-weight: 500;
+const StyledDiv = styled.div`
+  padding: 40px;
 `
+
 function App() {
 
 
   return (
     <>
-    <H1>I am a styled component</H1>
+    <GlobalStyles />
+    <StyledDiv>
+    <Heading as='h1'>I am a styled component</Heading>
+    <Heading as='h2'>Check in & Check out</Heading>
+    <Button variation="primary" size="medium">I am a button</Button>
+    <Button variation="secondary" size="small">I am another button</Button>
+    <Heading as='h3'>Form</Heading>
+    <Input />
+    </StyledDiv>
     </>
   )
 }
