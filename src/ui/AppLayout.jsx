4baf077372c03dfa0ffe714c1bf8b6da17589653
subikstha <subsatch@gmail.com>
2024-06-22 21:02:03 +0545

    sidebar and main navigation completed

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,11 +1,29 @@
 import { Outlet } from "react-router-dom"
+import Header from './Header'
+import Sidebar from './Sidebar'
+import styled from "styled-components"
+
+const StyledAppLayout = styled.div`
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100vh;
+`
+
+const Main = styled.main`
+  background-color: var(--color-grey-50);
+  padding: 4rem 4.8rem 6rem;
+`
 
 const AppLayout = () => {
   return (
-    <div>
-        <p>App Layout</p>
-        <Outlet />
-    </div>
+    <StyledAppLayout>
+        <Header />
+        <Sidebar />
+        <Main>
+          <Outlet />
+        </Main>
+    </StyledAppLayout>
   )
 }
 
